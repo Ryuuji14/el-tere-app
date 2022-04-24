b35afd344e7a0f535b7994000c96983cb65f37a4
Ruben Gutierrez <rubdeivis@gmail.com>
2022-04-24 19:37:12 -0400

    seach commerces

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef, useMemo, useState } from "react";
 import {
   Badge,
   Button,
@@ -13,20 +13,20 @@ import {
   Heading,
   Image,
   Icon,
-  ScrollView
+  ScrollView,
 } from "native-base";
-import { StyleSheet, Dimensions } from 'react-native';
-import Logo from '../../assets/LOGO-EL-TERE.png'
+import { StyleSheet, Dimensions } from "react-native";
+import Logo from "../../assets/LOGO-EL-TERE.png";
 import {
   FontAwesome,
   MaterialIcons,
   MaterialCommunityIcons,
 } from "@expo/vector-icons";
-import ComercioCard from '../components/screens/ComercioCard';
-import PromocionCard from '../components/screens/PromocionCard';
-import BottomNavigation from '../navigation/BottomNavigation';
+import ComercioCard from "../components/screens/ComercioCard";
+import PromocionCard from "../components/screens/PromocionCard";
+import BottomNavigation from "../navigation/BottomNavigation";
 
-import promociones from '../../assets/promociones.json'
+import promociones from "../../assets/promociones.json";
 
 var { height } = Dimensions.get("window");
 
@@ -35,7 +35,7 @@ const ICONS_PROPS = {
   color: "black",
   mr: 3,
 };
-const comercios = require('../../assets/comercios.json');
+const comercios = require("../../assets/comercios.json");
 
 const INPUT_PROPS = {
   borderColor: "#F96332",
@@ -45,52 +45,65 @@ const INPUT_PROPS = {
   variant: "rounded",
 };
 
-const categorias =
-  [
-    {
-      id: 0,
-      name: "Todos"
-    },
-    {
-      id: 1,
-      name: "Populares"
-    },
-    {
-      id: 2,
-      name: "Recientes"
-    },
-    {
-      id: 3,
-      name: "Recomendados"
-    },
-    {
-      id: 4,
-      name: "Orgia llanera"
-    },
-  ]
+const categorias = [
+  {
+    id: 0,
+    name: "Todos",
+  },
+  {
+    id: 1,
+    name: "Populares",
+  },
+  {
+    id: 2,
+    name: "Recientes",
+  },
+  {
+    id: 3,
+    name: "Recomendados",
+  },
+  {
+    id: 4,
+    name: "Orgia llanera",
+  },
+];
 
 const Dashboard = () => {
+  const [search, setSearch] = useState("");
+  const [searchBy, setSearchBy] = useState("");
 
-  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(0)
+  const [categoriaSeleccionada, setCategoriaSeleccionada] = useState(0);
+
+  const debounceRef = useRef(null);
+
+  const handleSearch = (val) => {
+    setSearch(val);
+    if (debounceRef.current) {
+      clearTimeout(debounceRef.current);
+    }
+
+    debounceRef.current = setTimeout(() => {
+      setSearchBy(val);
+    }, 350);
+  };
+
+  const filteresCommerces = useMemo(() => {
+    return comercios.filter((comercio) =>
+      comercio.name.toLowerCase().includes(searchBy.toLowerCase())
+    );
+  }, [comercios, searchBy]);
 
   return (
     <ScrollView>
-      <View
-        bgColor='white'
-        px={7}
-        pb={5}
-      >
+      <View bgColor="white" px={7} pb={5}>
         <Stack>
-          <HStack space={3} alignItems='center'>
-            <Image source={Logo}
-              alt='logo'
-              size='sm'
-            />
+          <HStack space={3} alignItems="center">
+            <Image source={Logo} alt="logo" size="sm" />
             <VStack>
-              <Heading color='#41634A' fontSize={20} fontWeight="bold" mt={5}>
+              <Heading color="#41634A" fontSize={20} fontWeight="bold" mt={5}>
                 ¡Bienvenido a EL TERE!
               </Heading>
-              <Text color='#c9c7c3' fontWeight="bold" fontSize={16} mb={4}>
+              <Text color="#c9c7c3" fontWeight="bold" fontSize={16} mb={4}>
                 Tu mejor aliado para hacer mercado
               </Text>
             </VStack>
@@ -101,7 +114,8 @@ const Dashboard = () => {
           <Stack>
             <HStack>
               <Input
-
+                value={search}
+                onChangeText={handleSearch}
                 {...INPUT_PROPS}
                 placeholder="Buscar..."
                 w="100%"
@@ -113,60 +127,69 @@ const Dashboard = () => {
           </Stack>
           <ScrollView horizontal showsHorizontalScrollIndicator={false}>
             <Stack>
-              <HStack mt='2' space={2}>
-                {
-                  categorias.map((elemento) => (
-                    <Button
-                      size='sm'
-                      key={elemento.id}
-                      my='2'
-                      bgColor={categoriaSeleccionada === elemento.id ? '#41634A' : 'white'}
-                      borderColor={categoriaSeleccionada === elemento.id ? '#41634A' : '#F96332'}
-                      borderWidth={1}
-                      borderRadius={50}
-                      onPress={() => setCategoriaSeleccionada(elemento.id)}
+              <HStack mt="2" space={2}>
+                {categorias.map((elemento) => (
+                  <Button
+                    size="sm"
+                    key={elemento.id}
+                    my="2"
+                    bgColor={
+                      categoriaSeleccionada === elemento.id
+                        ? "#41634A"
+                        : "white"
+                    }
+                    borderColor={
+                      categoriaSeleccionada === elemento.id
+                        ? "#41634A"
+                        : "#F96332"
+                    }
+                    borderWidth={1}
+                    borderRadius={50}
+                    onPress={() => setCategoriaSeleccionada(elemento.id)}
+                  >
+                    <Text
+                      color={
+                        categoriaSeleccionada === elemento.id ? "white" : "grey"
+                      }
                     >
-                      <Text
-                        color={categoriaSeleccionada === elemento.id ? 'white' : 'grey'}
-                      > {elemento.name} </Text>
-                    </Button>
-                  ))
-                }
+                      {" "}
+                      {elemento.name}{" "}
+                    </Text>
+                  </Button>
+                ))}
               </HStack>
             </Stack>
           </ScrollView>
 
           <Stack>
             <VStack>
-              <Text pt={2} pb={2} bold color='grey'>
+              <Text pt={2} pb={2} bold color="grey">
                 Promociones y Eventos
               </Text>
               <ScrollView horizontal showsHorizontalScrollIndicator={false}>
                 <HStack space={2}>
-                  {
-                    promociones.map((element) => (
-                      <PromocionCard
-                        key={element.id}
-                        image={element.image}
-                        name={element.name}
-                        description={element.description}
-                      />
-                    ))
-                  }
+                  {promociones.map((element) => (
+                    <PromocionCard
+                      key={element.id}
+                      image={element.image}
+                      name={element.name}
+                      description={element.description}
+                    />
+                  ))}
                 </HStack>
               </ScrollView>
             </VStack>
           </Stack>
-          <Text pt={2} pb={2} bold color='grey'>
+          <Text pt={2} pb={2} bold color="grey">
             Comercios
           </Text>
 
           <View>
             <FlatList
-              columnWrapperStyle={{ justifyContent: 'space-between' }}
+              columnWrapperStyle={{ justifyContent: "space-between" }}
               numColumns={2}
-              data={comercios}
-              renderItem={({ item }) =>
+              data={filteresCommerces}
+              renderItem={({ item }) => (
                 <ComercioCard
                   key={item.id}
                   id={item.id}
@@ -175,15 +198,14 @@ const Dashboard = () => {
                   type={item.type}
                   rating={item.rating}
                 />
-              }
-              keyExtractor={item => item.name}
+              )}
+              keyExtractor={(item) => item.name}
             />
           </View>
         </KeyboardAvoidingView>
       </View>
     </ScrollView>
-  )
-
-}
+  );
+};
 
 export default Dashboard;
