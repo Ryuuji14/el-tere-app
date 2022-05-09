069985693530a8bf715a9d0d15e2e5549f73f3fa
Ruben Gutierrez <rubdeivis@gmail.com>
2022-05-09 13:47:08 -0400

    cart swippe

--- src/screens/Cart/Cart.js
+++ src/screens/Cart/Cart.js
@@ -1,61 +1,89 @@
-import { Container, FlatList, Box, List } from 'native-base';
+import {
+  Container,
+  Image,
+  FlatList,
+  Box,
+  List,
+  Pressable,
+  Text,
+  HStack,
+  View,
+} from "native-base";
 import {
   StyleSheet,
-  Text,
   TouchableOpacity,
   TouchableHighlight,
-  View,
   Dimensions,
-  Image, 
-  Button
-} from 'react-native';
-import { ListItem } from 'react-native-elements';
+  Button,
+} from "react-native";
+import { ListItem } from "react-native-elements";
 
-import { SwipeListView } from 'react-native-swipe-list-view';
+import { SwipeListView } from "react-native-swipe-list-view";
 
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import * as actions from "../../Redux/Actions/cartActions";
 
-var { height, width } = Dimensions.get("window")
+var { height, width } = Dimensions.get("window");
 
 const Cart = (props) => {
-
-  var total=0;
-  props.cartItems.forEach(cart => {
-    return (total += cart.product.price)
-  });
+  var total = 0;
+  // props.cartItems.forEach(cart => {
+  //   return (total += cart.product.price)
+  // });
   // const [listData, setListData] = useState(
   //    Array(20)
   //        .fill('')
   //        .map((_, i) => ({ key: `${i}`, text: `${i}` }))
   // );
 
-
-  console.log(props.cartItems)
   return (
     <>
       {props.cartItems.length ? (
-        <Container backgroundColor='white'>
+        <View backgroundColor="white" width={width} h="100%">
           <Text style={{ alignSelf: "center" }}>Carrito</Text>
-          {props.cartItems.map(data => {
-            return (
-              <ListItem
-                key={data.product.id}
-                leftAvatar={{ source: { uri: data.product.image } }}
-                tittle={data.product.name}
-                bottomDivider
-              />
-            )
-          })}
+          <SwipeListView
+            data={props.cartItems || []}
+            renderItem={(data) => (
+              <Pressable h="10" bgColor="white">
+                <HStack>
+                  <Image
+                    w="10"
+                    h="10"
+                    source={{
+                      uri: data.item.product?.image,
+                    }}
+                  />
+                  <Text>{data?.item?.product?.id}</Text>
+                </HStack>
+              </Pressable>
+            )}
+            ItemSeparatorComponent={() => <View h="5" />}
+            renderHiddenItem={() => (
+              <Pressable
+                h="10"
+                bgColor="red.300"
+                onPress={() => console.log("ooh me vengo")}
+              >
+                <Text textAlign="right">Tocame sapo</Text>
+              </Pressable>
+            )}
+            rightOpenValue={-130}
+            disableRightSwipe
+            previewRowKey={"0"}
+            previewOpenValue={-40}
+            previewOpenDelay={3000}
+            onRowDidOpen={() => null}
+          />
+
           <View style={styles.bottomContainer}>
-              <Text style={styles.price}>${total}</Text>
-              <Button title="Limpiar" onPress={() => props.clearCart()} /> 
-              <Button
-                title="Comprar"
-               // onPress={() => props.navigation.navigate("Checkout")}
-              />
+            <Text style={styles.price}>${total}</Text>
+            <Button title="Limpiar" onPress={() => props.clearCart()} />
+            <Button
+              title="Comprar"
+              // onPress={() => props.navigation.navigate("Checkout")}
+            />
           </View>
-        </Container>
+        </View>
       ) : (
         <Container style={styles.emptyContainer}>
           <Text>Tu carrito esta vacío</Text>
@@ -63,34 +91,21 @@ const Cart = (props) => {
         </Container>
       )}
     </>
-    // <View style={styles.container}>
-    //   <SwipeListView
-    //        data={listData}
-    //        renderItem={renderItem}
-    //        renderHiddenItem={renderHiddenItem}
-    //        leftOpenValue={75}
-    //        rightOpenValue={-150}
-    //        previewRowKey={'0'}
-    //       previewOpenValue={-40}
-    //        previewOpenDelay={3000}
-    //        onRowDidOpen={onRowDidOpen}
-    //    />
-    //  </View>
   );
-}
+};
 
 const mapStateToProps = (state) => {
   const { cartItems } = state;
   return {
     cartItems: cartItems,
-  }
-}
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    clearCart: () => dispatch(actions.clearCart())
-  }
-}
+    clearCart: () => dispatch(actions.clearCart()),
+  };
+};
 
 const styles = StyleSheet.create({
   emptyContainer: {
@@ -116,14 +131,14 @@ const styles = StyleSheet.create({
   container: {
     height: height,
     width: width,
-    backgroundColor: 'white',
+    backgroundColor: "white",
     flex: 1,
   },
   listItem: {
-    alignItems: 'center',
-    backgroundColor: 'white',
-    justifyContent: 'center'
-  }
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Cart);
