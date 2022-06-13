22d0900843c9339561dea755cf42b14fea805aef
Ruben Gutierrez <rubdeivis@gmail.com>
2022-06-13 15:19:36 -0400

    categoria filter in dashboard

--- src/api/companyAPI.js
+++ src/api/companyAPI.js
@@ -4,7 +4,19 @@ const BASE_URL = "company";
 
 export const companyAPI = {
   getAreasWithProducts: async () => {
-    const { data } = await http.get("company/all/list");
+    const { data } = await http.get(`${BASE_URL}/all/list`);
+    return data;
+  },
+  getAreasWithRecommendedProducts: async () => {
+    const { data } = await http.get(`${BASE_URL}/remmended/list`);
+    return data;
+  },
+  getAreasWithPopularProducts: async () => {
+    const { data } = await http.get(`${BASE_URL}/popular/list`);
+    return data;
+  },
+  getAreasWithRecentProducts: async (userId) => {
+    const { data } = await http.get(`${BASE_URL}/recents/${userId}/list`);
     return data;
   },
   getCompanyProduct: async (companyId) => {
@@ -12,8 +24,7 @@ export const companyAPI = {
     return data;
   },
   getCompanyPromotions: async (companyId) => {
-    const {data} =  await http.get(`${'promotion/company'}/${companyId}`)
+    const { data } = await http.get(`${"promotion/company"}/${companyId}`);
     return data;
-  }
-
+  },
 };
